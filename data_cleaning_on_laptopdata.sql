d53383ae88a2a30a13899d1191bfe9a8ffcbb4eb
SaimanGiri <155413377+SaimanGiri@users.noreply.github.com>
2025-02-13 18:50:47 +0530

    Add files via upload

--- data_cleaning_on_laptopdata.sql
+++ data_cleaning_on_laptopdata.sql
@@ -1,9 +1,11 @@
 USE test_db;
 SELECT * FROM laptopdata;
 
+
 SELECT DATA_LENGTH/1024 FROM information_schema.TABLES
 WHERE TABLE_SCHEMA = 'test_db'
 AND TABLE_NAME = 'laptopdata';
+
 
 DELETE FROM laptopdata
 WHERE 
@@ -13,12 +15,15 @@ WHERE
 
 ALTER TABLE laptopdata MODIFY COLUMN Inches DECIMAL(3,1);
 
+
 UPDATE laptopdata
 SET Weight = (SELECT REPLACE(Weight, 'kg', ''));
+
 
 UPDATE laptopdata
 SET Price = (SELECT CEIL(Price));
 ALTER TABLE laptopdata MODIFY Price INTEGER;
+
 
 UPDATE laptopdata
 SET OpSys = (
@@ -36,21 +41,27 @@ ALTER TABLE laptopdata
 ADD COLUMN gpu_brand VARCHAR(255) AFTER Gpu,
 ADD COLUMN gpu_name VARCHAR(255) AFTER gpu_brand;
 
+
 UPDATE laptopdata
 SET gpu_brand = (SELECT SUBSTRING_INDEX(Gpu, ' ',1));
+
 
 UPDATE laptopdata
 SET gpu_name = (SELECT REPLACE(Gpu, gpu_brand, ''));
 
+
 ALTER TABLE laptopdata DROP COLUMN Gpu;
+
 
 ALTER TABLE laptopdata
 ADD COLUMN cpu_brand VARCHAR(255) AFTER Cpu,
 ADD COLUMN cpu_name VARCHAR(255) AFTER cpu_brand,
 ADD COLUMN cpu_speed VARCHAR(255) AFTER cpu_name;
 
+
 UPDATE laptopdata
 SET cpu_brand = (SELECT SUBSTRING_INDEX(Cpu, ' ', 1));
+
 
 UPDATE laptopdata
 SET cpu_speed = (SELECT SUBSTRING_INDEX(Cpu, ' ', -1));
@@ -58,30 +69,39 @@ SET cpu_speed = (SELECT SUBSTRING_INDEX(Cpu, ' ', -1));
 UPDATE laptopdata
 SET cpu_speed = (SELECT CAST(REPLACE(cpu_speed, 'GHz', '') AS DECIMAL(10,1)));
 
+
 ALTER TABLE laptopdata MODIFY COLUMN Ram INTEGER;
+
 
 ALTER TABLE laptopdata
 ADD COLUMN screen_width INTEGER AFTER ScreenResolution,
 ADD COLUMN screen_height INTEGER AFTER screen_width;
 
+
 UPDATE laptopdata
 SET screen_width = (SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(ScreenResolution, ' ', -1),'x', 1)),
 screen_height = (SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(ScreenResolution, ' ', -1),'x', -1));
 
+
 ALTER TABLE laptopdata
 ADD COLUMN touchscreen INTEGER AFTER screen_height;
+
 
 UPDATE laptopdata
 SET touchscreen = (SELECT CASE WHEN ScreenResolution LIKE '%touch%' THEN 1 ELSE 0 END);
 
+
 ALTER TABLE laptopdata DROP COLUMN ScreenResolution;
 
+
 SELECT DISTINCT Memory FROM laptopdata;
+
 
 ALTER TABLE laptopdata
 ADD COLUMN memory_type VARCHAR(255) AFTER Memory,
 ADD COLUMN primary_storage INTEGER AFTER memory_type,
 ADD COLUMN secondary_storage INTEGER AFTER primary_storage;
+
 
 UPDATE laptopdata
 SET memory_type = (SELECT CASE 
@@ -93,15 +113,19 @@ SET memory_type = (SELECT CASE
     ELSE null
 END );
 
+
 UPDATE laptopdata
 SET primary_storage = (SELECT REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+', 1),'[0-9]+')),
 secondary_storage = (SELECT CASE WHEN memory_type = 'Hybrid' THEN REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+', -1),'[0-9]+') ELSE 0 END);
+
 
 UPDATE laptopdata
 SET primary_storage = (SELECT CASE WHEN primary_storage <= 2 THEN primary_storage*1024 ELSE primary_storage END),
 secondary_storage = (SELECT CASE WHEN secondary_storage <= 2 THEN secondary_storage*1024 ELSE secondary_storage END);
 
+
 ALTER TABLE laptopdata DROP COLUMN Memory;
+
 
 UPDATE laptopdata
 SET secondaRy_storage = (SELECT CASE WHEN memory_type = 'Hybrid' THEN secondary_storage ELSE 0 END);
